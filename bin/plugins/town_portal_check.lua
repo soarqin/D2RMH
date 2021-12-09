3d1c570a7da56d470176d5db02ec7560add7e82c
Jack Cherng <jfcherng@gmail.com>
2021-12-09 17:33:28 +0800

    fix should not checking portal during loading screen (#88)
    * fix should not checking portal during loading screen
    
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>
    
    * improve town portal check message
    
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- bin/plugins/town_portal_check.lua
+++ bin/plugins/town_portal_check.lua
@@ -1,9 +1,14 @@
 local text = create_text_list("default")
 
 local town_portal_check = function()
-    local skill = get_skill(220)
-    if not skill or skill.quantity < 3 then
-        text:add("\x0BWARNING! Town portal scrolls quantity low", 1500, 0)
+    -- game fully loaded?
+    if not get_skill(0) then
+        return
+    end
+
+    local portal_book = get_skill(220)
+    if portal_book.quantity < 3 then
+        text:add(string.format("\x0BWARNING! Town portal scrolls quantity low: %d", portal_book.quantity), 1500, 0)
     end
 end
 register_plugin(1000, town_portal_check)
